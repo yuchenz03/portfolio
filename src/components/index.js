79da969bba1961fbbf623eb4512352d0fafa26b4
mermer03 <109605191+yuchenz03@users.noreply.github.com>
2026-02-16 03:19:30 -0500

    added github pages workflow

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Main } from "./Main";
 export { default as About } from "./About";
 export { default as Timeline } from "./Timeline";
 export { default as Project } from "./Project";
-export { default as Contact } from "./Contact";
+export { default as Contact } from "./Contact2";
